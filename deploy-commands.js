204acc0a332afc25b3413f88c52c063f8c2e1faf
Napp <83326679+AetCloud@users.noreply.github.com>
2025-02-12 14:58:04 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,8 +4,19 @@ const commands = require("./commands");
 
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
+const guildId = "1146990138656825415"; // Your Server ID
 
 const rest = new REST({ version: "10" }).setToken(token);
+
+(async () => {
+  try {
+    console.log("🚨 Clearing all commands...");
+    await rest.put(Routes.applicationCommands(clientId), { body: [] });
+    console.log("✅ Cleared all commands!");
+  } catch (error) {
+    console.error("❌ Failed to clear commands:", error);
+  }
+})();
 
 (async () => {
   try {
