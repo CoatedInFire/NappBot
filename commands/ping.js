5cac3ae307c402b9bf5bd0f8286c378ac8ada08b
Napp <83326679+AetCloud@users.noreply.github.com>
2025-02-13 18:39:03 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,13 +4,14 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("🏓 Pings the bot and shows the latency.")
-    .setContexts(Contexts.Guild | Contexts.DirectMessage),
+    .setContexts([Contexts.Guild, Contexts.BotDM]),
+
   async execute(interaction) {
     await interaction.reply({
       content: `🏓 Pong! Latency: ${
         Date.now() - interaction.createdTimestamp
       }ms`,
-      ephemeral: true, // ✅ Private response
+      ephemeral: true,
     });
   },
 };
