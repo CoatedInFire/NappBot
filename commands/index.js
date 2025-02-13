433b5f27e51691d72d4a1019c09d6bf8df26a510
Napp <83326679+AetCloud@users.noreply.github.com>
2025-02-14 00:17:08 +0800

    Updated index

---
+++ commands/index.js
@@ -0,0 +1,14 @@
+// commands/index.js
+module.exports = [
+  require("./cmds.js"),
+  require("./e621.js"),
+  require("./e621profile.js"),
+  require("./fuck.js"),
+  require("./hug.js"),
+  require("./kiss.js"),
+  require("./lick.js"),
+  require("./ping.js"),
+  require("./setpreference.js"),
+  require("./settings.js"),
+  require("./vp.js"),
+];
