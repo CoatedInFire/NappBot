aff304c7cd7f9856693c54f00b911c45e26fcf33
Napp <83326679+AetCloud@users.noreply.github.com>
2025-02-18 22:58:12 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("🏓 Pings the bot and shows the latency."),
+    .setDescription("🏓 Pings the bot and shows the latency.")
+    .setDMPermission(true),
   async execute(interaction) {
     await interaction.reply({
       content: `🏓 Pong! Latency: ${
