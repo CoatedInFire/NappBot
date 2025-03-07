fe99686134abef159cab8fcecb63c3fe36770e48
Napp <83326679+AetCloud@users.noreply.github.com>
2025-03-07 23:36:04 +0800

    Delete example.js

--- commands/example.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("example")
-    .setDescription("Example command"),
-  async execute(interaction) {
-    await interaction.reply("Example command executed!");
-  },
-  modulePath: __filename,
-};
